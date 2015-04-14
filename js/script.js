fde9d13d4ca589d9ee5ffbd0eafd51038dd0aeea
Cynthia Wilhelm & Kenna Warsinske <student@epicodus.com>
2015-04-14 16:44:04 -0700

    Front-end tweaks

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var newName = $("input#name").val();
 
     var newPet = {name: newName,
-    food: 100
+    food: 10
     };
     var myVar=setInterval(function(){myTimer()},1000);
     var foodLevel;
@@ -17,12 +17,14 @@ $(document).ready(function() {
         if (newPet.food < 25) {
           $(".med." + newPet.name).show();
         }
-        if (newPet.food < 0) {
+        if (newPet.food === 0) {
           $("li." + newPet.name).addClass("dead");
+          $("#deathList").append("<li> &#9760" + newPet.name + "</li>");
+          $("#cemetary").show();
         }
     }
 
-    $("#petList").append("<li class='" + newPet.name + "'><span class='glyphicon glyphicon-plus clickable med " + newPet.name + "'></span><span class='glyphicon glyphicon-cutlery clickable feed" + newPet.name + "'></span> " + newPet.name + " energy level: " + "<span id='" + newPet.name + "'></span> " + "</li>");
+    $("#petList").append("<h3><li class='" + newPet.name + "'><span class='glyphicon glyphicon-plus clickable med " + newPet.name + "'></span><span class='glyphicon glyphicon-cutlery clickable feed" + newPet.name + "'></span> " + newPet.name + " energy level: " + "<span id='" + newPet.name + "'></span> " + "</li></h3>");
 
     $(".feed" + newPet.name).click(function() {
       newPet.food += 25;
